e82fc1b4e93de4663d45da31aadfba3a40ecb9ac
liuwei <18392419211@163.com>
2022-04-15 09:13:08 +0800

    103.ZigZigLevelOrder Pass

--- src/103.ZigZigLevelOrder.cc
+++ src/103.ZigZigLevelOrder.cc
@@ -2,19 +2,24 @@
 
 class Solution {
  public:
-  vector<vector<int>> levelOrder(TreeNode* root) {
+  vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
     if (!root) return {};
-    
+
     queue<TreeNode*> queue;
     vector<vector<int>> res;
     queue.push(root);
+
+    bool leftToRight = true;
     while (!queue.empty()) {
       int size = queue.size();
-      vector<int> level;
-      while (size > 0) {
+      vector<int> level(size);
+
+      for (int i = 0; i < size; i++) {
         TreeNode* node = queue.front();
         queue.pop();
-        level.push_back(node->val);
+
+        int index = leftToRight ? i : size - i - 1;
+        level[index] = node->val;
 
         if (node->left) {
           queue.push(node->left);
@@ -23,39 +28,39 @@ class Solution {
         if (node->right) {
           queue.push(node->right);
         }
-
-        size--;
       }
+      leftToRight = !leftToRight;
       res.push_back(level);
     }
 
     return res;
   }
 };
+
 static const auto speedup = []() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   return 0;
 }();
 
-TEST(BinaryTreeLevelOrderTraversal, BinaryTreeLevelOrderTraversal_1) {
+TEST(ZigZigLevelOrder, ZigZigLevelOrder_1) {
   Solution s;
   Tree tree{3, 9, 20, NULL, NULL, 15, 7};
-  vector<vector<int>> ans{{3}, {9, 20}, {15, 7}};
-  EXPECT_EQ(s.levelOrder(tree.root), ans);
+  vector<vector<int>> ans = {{3}, {20, 9}, {15, 7}};
+  EXPECT_EQ(s.zigzagLevelOrder(tree.root), ans);
 }
 
-TEST(BinaryTreeLevelOrderTraversal, BinaryTreeLevelOrderTraversal_2) {
+TEST(ZigZigLevelOrder, ZigZigLevelOrder_2) {
   Solution s;
   Tree tree{1};
-  vector<vector<int>> ans{{1}};
-  EXPECT_EQ(s.levelOrder(tree.root), ans);
+  vector<vector<int>> ans = {{1}};
+  EXPECT_EQ(s.zigzagLevelOrder(tree.root), ans);
 }
 
-TEST(BinaryTreeLevelOrderTraversal, BinaryTreeLevelOrderTraversal_3) {
+TEST(ZigZigLevelOrder, ZigZigLevelOrder_3) {
   Solution s;
-  vector<vector<int>> ans{};
-  EXPECT_EQ(s.levelOrder(nullptr), ans);
+  vector<vector<int>> ans = {};
+  EXPECT_EQ(s.zigzagLevelOrder(nullptr), ans);
 }
 
 int main(int argc, char** argv) {
